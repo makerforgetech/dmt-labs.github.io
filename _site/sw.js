be66079048796cf568ef788d7e15d1acef7fd342
danic85 <dan.nicholson@outlook.com>
2023-10-07 17:43:07 +0100

    Change url

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231006.185351';
+const cacheName = 'chirpy-20231007.174258';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
