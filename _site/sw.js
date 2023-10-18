59ea709529ea353c556155e7058f02e565cf5dee
danic85 <dan.nicholson@outlook.com>
2023-10-18 07:20:00 +0100

    Update

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231017.102326';
+const cacheName = 'chirpy-20231018.071933';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
